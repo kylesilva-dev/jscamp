f3231ded097267909ac7c20be25cf9501765247e
Mercy <silvakyle075@gmail.com>
2025-10-09 14:49:23 -0500

    ASDASD

---
+++ 00-html-css/search/script.js
@@ -0,0 +1,13 @@
+/*
+const botones = document.querySelectorAll('.button-apply-job')
+
+botones.forEach(boton => {
+     boton.addEventListener('click', () => {
+        boton.textContent = 'Aplicado!'
+        boton.classList.add('is-applied')
+        boton.disabled = true
+        }
+    )
+}
+)
+*/
